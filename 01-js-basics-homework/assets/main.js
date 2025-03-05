efce31fb5ac849e18d5aa1f65c5a81d96e4fe213
Edward <110572334+EdwardN93@users.noreply.github.com>
2025-03-05 22:53:25 +0200

    refactored a bit of code

--- 01-js-basics-homework/assets/main.js
+++ 01-js-basics-homework/assets/main.js
@@ -9,64 +9,60 @@ const btnCreateTeams = document.querySelector(".noOfTeams");
 btnCalcAttendance.style.display = "none";
 
 const students = [
-  //   {
-  //     name: "Bombastic",
-  //     age: 31,
-  //     attendance: true,
-  //   },
-  //   {
-  //     name: "Iso",
-  //     age: 31,
-  //     attendance: false,
-  //   },
-  //   {
-  //     name: "Martin",
-  //     age: 31,
-  //     attendance: true,
-  //   },
-  //   {
-  //     name: "Garrix",
-  //     age: 32,
-  //     attendance: true,
-  //   },
-  //   {
-  //     name: "Edward",
-  //     age: 21,
-  //     attendance: false,
-  //   },
-  //   {
-  //     name: "Aniamls",
-  //     age: 31,
-  //     attendance: true,
-  //   },
-  //   {
-  //     name: "Trixie",
-  //     age: 31,
-  //     attendance: true,
-  //   },
-  //   {
-  //     name: "Fanny",
-  //     age: 31,
-  //     attendance: true,
-  //   },
-  //   {
-  //     name: "Combi",
-  //     age: 31,
-  //     attendance: true,
-  //   },
-  //   {
-  //     name: "Jon",
-  //     age: 31,
-  //     attendance: true,
-  //   },
+  // {
+  //   name: "Bombastic",
+  //   age: 31,
+  //   attendance: true,
+  // },
+  // {
+  //   name: "Iso",
+  //   age: 31,
+  //   attendance: false,
+  // },
+  // {
+  //   name: "Martin",
+  //   age: 31,
+  //   attendance: true,
+  // },
+  // {
+  //   name: "Garrix",
+  //   age: 32,
+  //   attendance: true,
+  // },
+  // {
+  //   name: "Edward",
+  //   age: 21,
+  //   attendance: false,
+  // },
+  // {
+  //   name: "Aniamls",
+  //   age: 31,
+  //   attendance: true,
+  // },
+  // {
+  //   name: "Trixie",
+  //   age: 31,
+  //   attendance: true,
+  // },
+  // {
+  //   name: "Fanny",
+  //   age: 31,
+  //   attendance: true,
+  // },
+  // {
+  //   name: "Combi",
+  //   age: 31,
+  //   attendance: true,
+  // },
+  // {
+  //   name: "Jon",
+  //   age: 31,
+  //   attendance: true,
+  // },
 ];
 
 function getStudent(name, age, attendance) {
-  return {
-    name: name,
-    age: age,
-    attendance: attendance,
-  };
+  return { name, age, attendance };
 }
 
 function addStudentToList(e) {
@@ -83,6 +79,11 @@ function addStudentToList(e) {
     document.querySelector(
       ".inputError"
     ).textContent = `Provide a valid name and age`;
+    return;
+  } else if (!attendance) {
+    document.querySelector(".errorMsg").style.display = "block";
+    document.querySelector(".inputError").textContent =
+      "Please select attendance";
     return;
   } else {
     document.querySelector(".errorMsg").style.display = "none";
@@ -107,28 +108,39 @@ function addStudentToList(e) {
   age.value = "";
 }
 
+function getPresentStudents() {
+  return students.filter((student) => student.attendance === "true");
+}
+
 function showAttendancePercentage(students) {
-  const attendance = students.filter(
-    (student) => student.attendance === "true"
-  ).length;
+  const attendance = getPresentStudents().length;
 
   return ((attendance / students.length) * 100).toFixed() + "%";
 }
 
-function shuffleStudents(numTeams) {
-  let teams = new Array(numTeams).fill([]);
-  const presentStudents = students.filter(
-    (student) => student.attendance === "true"
-  );
+function shuffleStudents(numOfTeams) {
+  let teams = new Array(numOfTeams).fill([]);
+  const presentStudents = getPresentStudents();
+
+  if (numOfTeams > getPresentStudents().length) {
+    let teamHTML = `
+  <div class='error'>
+    <p>Max number of teams you can create: ${getPresentStudents().length}</p>
+  </div>
+  `;
+    noTeams.insertAdjacentHTML("beforeend", teamHTML);
+
+    return;
+  }
 
   let nameOfStudents = presentStudents.map((student) => student.name);
 
   nameOfStudents = nameOfStudents.sort(() => Math.random() - 0.5);
 
-  const teamSize = Math.floor(nameOfStudents.length / numTeams);
-  const remainingStudents = nameOfStudents.length % numTeams;
+  const teamSize = Math.floor(nameOfStudents.length / numOfTeams);
+  const remainingStudents = nameOfStudents.length % numOfTeams;
 
-  for (let i = 0; i < numTeams; i++) {
+  for (let i = 0; i < numOfTeams; i++) {
     const extraStudent = i < remainingStudents ? 1 : 0;
     teams[i] = nameOfStudents.splice(0, teamSize + extraStudent);
   }
@@ -137,30 +149,25 @@ function shuffleStudents(numTeams) {
   noTeams.innerHTML = "";
 
   teams.forEach((team, index) => {
-    const div = document.createElement("div");
-    div.classList.add(`team-${index + 1}`);
+    console.log(team, index);
+    const teamIndex = index + 1;
+    let teamHTML = `
+      <div class="team-${teamIndex}">
+      <h2>Team ${teamIndex}</h2>
+      <ul>
+      ${team.map((student) => `<li>${student}</li>`).join("")}
+      </ul>
+      </div>
+      `;
 
-    const h2 = document.createElement("h2");
-    h2.textContent = `Team ${index + 1}`;
-    div.appendChild(h2);
-
-    const ul = document.createElement("ul");
-    div.appendChild(ul);
-
-    team.forEach((student) => {
-      const li = document.createElement("li");
-      li.textContent = student;
-      ul.appendChild(li);
-    });
-
-    noTeams.appendChild(div);
+    noTeams.insertAdjacentHTML("beforeend", teamHTML);
   });
 }
 
 function displayCalcPercentage() {
-  return (document.querySelector(
+  document.querySelector(
     ".attendance-percentage"
-  ).innerText = `Presence was: ${showAttendancePercentage(students)}`);
+  ).innerText = `Presence was: ${showAttendancePercentage(students)}`;
 }
 
 // Optional for hiding and showing form?
@@ -173,9 +180,9 @@ function displayCalcPercentage() {
 btnCreateTeams.addEventListener("click", function (e) {
   e.preventDefault();
   e.stopPropagation();
-  ``;
+
   const numTeams = document.querySelector(".numTeams");
-  shuffleStudents(numTeams.value || 3);
+  shuffleStudents(numTeams.value || 1);
   numTeams.value = "";
 });
 
