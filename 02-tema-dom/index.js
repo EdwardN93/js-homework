32a6dd8a9d223537d32e932882b7d2a51d3c007a
Edward <110572334+EdwardN93@users.noreply.github.com>
2025-03-18 18:02:38 +0200

    changes in code for better readability and optimization

--- 02-tema-dom/index.js
+++ 02-tema-dom/index.js
@@ -1,5 +1,11 @@
+const defaultAvatar = `https://upload.wikimedia.org/wikipedia/commons/5/59/User-avatar.svg`;
+
 const studentNameInput = document.querySelector("#student-name");
 const studentColor = document.querySelector("#student-color");
+
+const pickedStudentDiv = document.querySelector("#picked-student-div");
+const pickedStudentImg = document.querySelector("#picked-student-img");
+const tbody = document.querySelector("tbody");
 
 // BUTTONS RERERENCE
 const btnAddStudent = document.querySelector("#add-student-btn");
@@ -22,13 +28,17 @@ btnClearList.disabled = true;
 
 const getStudentName = (e) => {
   e.preventDefault();
+
   const studentName = studentNameInput.value.trim();
   if (studentName.length < 1) return alert("Provide a valid name");
+
   btnClearList.disabled = false;
-  const html = `
+  const row = document.createElement("tr");
+  row.innerHTML = `
     <tr>
-        <td class='name-list'>
+        <td class='name-list' data-student-name="${studentName}">
             <img
+                data-border-clr="${studentColor.value}"
                 src="https://api.dicebear.com/9.x/avataaars-neutral/svg?seed=${studentName}
                 alt="avatar"
                 class="img-thumbnail me-2"      
@@ -40,65 +50,72 @@ const getStudentName = (e) => {
         </td>
     </tr>
   `;
-  const tbody = document.querySelector("tbody");
-  tbody.insertAdjacentHTML("beforeend", html);
+  tbody.appendChild(row);
 
-  const newImage = document.querySelector("tbody tr:last-child .img-thumbnail");
-  newImage.style.borderColor = `${studentColor.value}`;
-  const newColor = document.querySelector("tbody tr:last-child .student-color");
-  newColor.style.backgroundColor = `${studentColor.value}`;
+  studentsColor(row, studentColor.value);
+
+  const deleteBtn = row.querySelector(".delete-btn");
+  deleteBtn.addEventListener("click", () => {
+    row.remove();
+    checkStudentsList();
+  });
 
   studentColor.value = randomStudentColor();
   studentNameInput.value = "";
 };
 
+const studentsColor = (row, color) => {
+  const newImage = row.querySelector("[data-border-clr]");
+  const newColor = row.querySelector(".student-color");
+
+  newImage.style.borderColor = `${color}`;
+  newColor.style.backgroundColor = `${color}`;
+};
+
 const checkStudentsList = () => {
-  if (document.querySelectorAll(".name-list").length === 0) {
+  if (tbody.children.length === 0) {
     resetPickedStudent();
   }
 };
 
 const resetPickedStudent = () => {
-  document.querySelector(
-    "#picked-student-div"
-  ).innerText = `Press the Button to pick a student`;
+  changeDetails();
 
-  document.querySelector(".img-thumbnail").style.border = `1px solid #dee2e6`;
-  const defaultAvatar = `https://upload.wikimedia.org/wikipedia/commons/5/59/User-avatar.svg`;
-  document.querySelector("#picked-student-img").src = defaultAvatar;
   btnClearList.disabled = true;
+};
+
+const changeDetails = (
+  text = `Press the Button to pick a student`,
+  border = `1px solid #dee2e6`,
+  source = defaultAvatar
+) => {
+  pickedStudentDiv.innerText = text;
+  pickedStudentImg.style.border = border;
+  pickedStudentImg.src = source;
 };
 
 // EVENT HANDLERS
 
 btnAddStudent.addEventListener("click", getStudentName);
 
-document.querySelector("tbody").addEventListener("click", (e) => {
-  if (e.target && e.target.classList.contains("delete-btn")) {
-    const row = e.target.closest("tr");
-    row.remove();
-    checkStudentsList();
-  }
-});
-
 btnPickStudent.addEventListener("click", () => {
-  const students = document.querySelectorAll(".name-list");
+  const students = tbody.querySelectorAll(".name-list");
 
   if (students.length === 0) return alert("No students to pick");
 
   const randomStudent = students[Math.floor(Math.random() * students.length)];
   const randomStudentImg = randomStudent.querySelector("img");
+  const randomStudentsName = randomStudent.dataset.studentName;
 
-  document.querySelector("#picked-student-div").innerText =
-    randomStudent.innerText;
-  document.querySelector(
-    ".img-thumbnail"
-  ).style.border = `5px solid ${randomStudentImg.style.borderColor}`;
-  document.querySelector("#picked-student-img").src = randomStudentImg.src;
+  changeDetails(
+    randomStudentsName,
+    `5px solid ${randomStudentImg.dataset.borderClr}`,
+    randomStudentImg.src
+  );
 });
 
 btnClearList.addEventListener("click", (e) => {
-  document.querySelector("tbody").innerHTML = "";
+  tbody.innerHTML = "";
   e.target.disabled = true;
   resetPickedStudent();
 });
